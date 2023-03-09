cfdb905a9e919d1874a0f344bd096a1eae5cea64
Ahinsa Kavindi <kavindijada.20@uom.lk>
2023-03-09 22:17:55 +0530

    Update

--- Client/src/App.js
+++ Client/src/App.js
@@ -11,7 +11,7 @@ function App() {
     
     <Routes>
      
-        <Route path="*" element={<Home/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/filteredBuses" element={<FilteredBuses/>} />
       
       </Routes>
