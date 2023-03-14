a2f21560af25e66f9961c77262fdf69b8edd0f1a
Chathura_Basnayaka <110757783+chathura1e2@users.noreply.github.com>
2023-03-14 18:31:38 +0530

    Update navbar

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Homepage from './pages/Homepage';
+import Aboutus from './pages/Aboutus';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar/> 
+      <Switch>
+        <Route path="/" exact component={Homepage}/>
+        <Route path="/Aboutus" component={Aboutus}/>
+
+      </Switch>
+
+    </Router>
   );
 }
 export default App;
