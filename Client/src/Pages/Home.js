3d380eaffbe5fa2e6f60ba0984dddf9da439eb69
Ahinsa Kavindi <kavindijada.20@uom.lk>
2023-06-13 19:12:29 +0530

    update

--- Client/src/Pages/Home.js
+++ Client/src/Pages/Home.js
@@ -1,11 +1,7 @@
-import Sidebar from "../Components/Sidebar";
-import Navbar from "../Components/Navbar";
 import "../Css/home.scss";
-import image from '../Css/back 3.jpeg'
 import image2 from "../Css/BUS_MANAGEMENT_SYSTEM.jpeg";
-import { useNavigate,Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import UnRegMap from "../Components/UnRegMap";
-
 
 const Home = () => {
 
@@ -15,31 +11,33 @@ const Home = () => {
     <div className="home">
       <div className="homeContainer">
         <div className="items">
-        <div className="item">
-        <div>
-        <Link to="/" style={{ textDecoration: "none" }}>
-            <img src={image2} className="icontop" />
-            </Link>
-          </div>
-          <div className="name"><p>TRAVO</p></div>
-        </div>
-    
-        <div className="item">
-            <button  onClick={()=> {navigate('/login')}}><span></span>Login</button>
-          </div>
+
           <div className="item">
-            <button  onClick={()=> {navigate('/register')}}><span></span>Sign Up</button>
+            <div>
+              {/* image source is set to the image2 variable */}
+              <img src={image2} className="icontop" />
+            </div>
+            <div className="name"><p>TRAVO</p></div>
           </div>
+
+          <div className="item">
+            <button onClick={() => { navigate('/login') }}><span></span>Login</button>
+          </div>
+
+          <div className="item">
+            <button onClick={() => { navigate('/register') }}><span></span>Sign Up</button>
+          </div>
+
         </div>
-       
+
         <div className="map">
-        <UnRegMap/>
+          <UnRegMap />
         </div>
-            
+
       </div>
 
-      
-      
+
+
     </div>
   );
 };
